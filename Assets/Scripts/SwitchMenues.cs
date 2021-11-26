510adffd38d6f42b593f1cf48fd86b1d259cedca
ksheesh <85945736+ksheesh@users.noreply.github.com>
2021-11-26 18:25:38 +0100

    [Update] Switch Config <--> Director
    * neue Buttons zum hin und her wechseln
    * Licht BugFix

---
+++ Assets/Scripts/SwitchMenues.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMenues : MonoBehaviour
+{
+    public GameObject ObjectMenueConfigMain;
+    public GameObject ObjectMenueDirectorMain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchToMenueDirector()
+    {
+        ObjectMenueConfigMain.SetActive(false);
+        ObjectMenueDirectorMain.SetActive(true);
+    }
+    public void SwitchToMenueConfig()
+    {
+        ObjectMenueDirectorMain.SetActive(false);
+        ObjectMenueConfigMain.SetActive(true);
+    }
+}
