bc5a82b2c2f109c8f96c26a88e29489cb5d0c727
Rainer Uhlemann <rainer.uhlemann@lightframefx.de>
2022-05-24 21:26:19 +0200

    add functionality of fullscreen-mode, add stop-button

--- Assets/Scripts/SwitchLiveView.cs
+++ Assets/Scripts/SwitchLiveView.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SwitchLiveView : MonoBehaviour
 {
+	public GameObject liveViewFullscreenObj;
+	public Button PlayBtn;
+	public Sprite PlayIcon;
+	public Sprite PauseIcon;
+	
     //// Start is called before the first frame update
     void Start()
     {
-        
+        SwitchToNormalsceen();
     }
 
     //// Update is called once per frame
@@ -18,13 +24,32 @@ public class SwitchLiveView : MonoBehaviour
 
     public void SwitchToFullscreen()
     {
-        //ObjectMenueConfigMain.SetActive(false);
-        //ObjectMenueDirectorMain.SetActive(true);
-        //StaticSceneData.Everything3D();
+		liveViewFullscreenObj.SetActive(true);
     }
     public void SwitchToNormalsceen()
     {
-        //ObjectMenueDirectorMain.SetActive(false);
-        //ObjectMenueConfigMain.SetActive(true);
+		liveViewFullscreenObj.SetActive(false);
     }
+	public void changePlayIcon()
+	{
+		//Debug.Log("timerstate: "+AnimationTimer.GetTimerState());
+		switch (AnimationTimer.GetTimerState())
+		{
+            case AnimationTimer.TimerState.stopped:
+                //AnimationTimer.StartTimer();
+                //SceneManaging.playing = true;
+                PlayBtn.transform.GetComponent<Image>().sprite = PauseIcon;
+                break;
+            case AnimationTimer.TimerState.playing:
+                //AnimationTimer.PauseTimer();
+                //SceneManaging.playing = false;
+                PlayBtn.transform.GetComponent<Image>().sprite = PlayIcon;
+                break;
+            case AnimationTimer.TimerState.paused:
+                //AnimationTimer.StartTimer();
+                //SceneManaging.playing = true;
+                PlayBtn.transform.GetComponent<Image>().sprite = PauseIcon;
+                break;
+            }
+	}
 }
