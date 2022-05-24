575fe7b433e00871adc28b83e568f4c7b1347921
Rainer Uhlemann <rainer.uhlemann@lightframefx.de>
2022-05-24 14:40:58 +0200

    replace menue-buttons, add fullscreen without function

---
+++ Assets/Scripts/SwitchLiveView.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchLiveView : MonoBehaviour
+{
+    //// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    public void SwitchToFullscreen()
+    {
+        //ObjectMenueConfigMain.SetActive(false);
+        //ObjectMenueDirectorMain.SetActive(true);
+        //StaticSceneData.Everything3D();
+    }
+    public void SwitchToNormalsceen()
+    {
+        //ObjectMenueDirectorMain.SetActive(false);
+        //ObjectMenueConfigMain.SetActive(true);
+    }
+}
