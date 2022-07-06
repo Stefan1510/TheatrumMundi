ad55ececa9ecb2bd46628caa1e652dc5e12deaf9
ksheesh <85945736+ksheesh@users.noreply.github.com>
2022-07-06 18:30:54 +0200

    [Update] DeactivateExpertTools
    * updated LoadSave Menu for Users

--- Assets/Scripts/UnitySwitchExpertUser.cs
+++ Assets/Scripts/UnitySwitchExpertUser.cs
@@ -16,6 +16,13 @@ public class UnitySwitchExpertUser : MonoBehaviour
     [SerializeField] private GameObject _menueKulissen;
     [SerializeField] private GameObject _buttonLadenSpeichern;
 
+    [SerializeField] private GameObject _buttonDelete;
+    [SerializeField] private GameObject _buttonSave;
+    [SerializeField] private GameObject _textSave;
+    [SerializeField] private GameObject[] _aInputFieldSave;
+    [SerializeField] private GameObject _buttonLoad;
+    [SerializeField] private GameObject _scrollViewFileSelect;
+
 
     private bool _gameControllerStarted = false;         
 
@@ -27,6 +34,18 @@ public class UnitySwitchExpertUser : MonoBehaviour
         {
             _buttonKulissen.GetComponent<RectTransform>().Translate(0, 137, 0);
             _buttonLadenSpeichern.GetComponent<RectTransform>().Translate(0, 2 * 137, 0);
+            _panelLightAmbient.SetActive(false);
+            _buttonDelete.SetActive(false);
+            _buttonSave.SetActive(false);
+            _textSave.SetActive(false);
+            foreach(GameObject inputFieldSave in _aInputFieldSave)
+            {
+                inputFieldSave.SetActive(false);
+            }
+            _menueKulissen.SetActive(true);
+            _buttonLoad.GetComponent<RectTransform>().Translate(0, -300, 0);
+            RectTransform rectScrollView = _scrollViewFileSelect.GetComponent<RectTransform>();
+            rectScrollView.sizeDelta = new Vector2(rectScrollView.sizeDelta.x, rectScrollView.sizeDelta.y + 300);
         }
     }
 
@@ -43,12 +62,10 @@ public class UnitySwitchExpertUser : MonoBehaviour
     {
         if (!_isExpert)
         {
-            _panelLightAmbient.SetActive(false);
             _buttonBuehne.SetActive(false);
             _menueBuehne.SetActive(false);
             _buttonLicht.SetActive(false);
             _menueLicht.SetActive(false);
-            _menueKulissen.SetActive(true);
         }
     }
 }
